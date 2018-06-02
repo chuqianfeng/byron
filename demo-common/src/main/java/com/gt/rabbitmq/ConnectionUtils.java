1049589d505495bd149a0d7f4fd54a3acd9ace15
cqf <byron_chu@163.com>
2018-06-03 02:10:21 +0800

    robbitmq

--- demo-common/src/main/java/com/gt/rabbitmq/ConnectionUtils.java
+++ demo-common/src/main/java/com/gt/rabbitmq/ConnectionUtils.java
@@ -16,11 +16,12 @@ public class  ConnectionUtils {
     public static Connection getConnection() throws IOException, TimeoutException {
 
         ConnectionFactory factory = new ConnectionFactory();
-        factory.setHost("192.168.1.5");
+        factory.setHost("192.168.1.4");
         factory.setPort(5672);
         factory.setVirtualHost("/vhost_mmr");
         factory.setUsername("user");
         factory.setPassword("1234");
+
 
         return factory.newConnection();
 
